a018b92abc9a814d862e8213c08591c3be130099
Julio Leon <juliopena143@icloud.com>
2024-11-25 15:43:59 -0700

    printing scores and winning player logic

--- Mini Python Projects/pig_game.py
+++ Mini Python Projects/pig_game.py
@@ -27,6 +27,7 @@ player_scores = [0 for _ in range(players)]
 while max(player_scores) < max_score:
     for player_index in range(players):
         print("\nPlayer Number", player_index + 1, "turn has just started!\n")
+        print("Your total score is:", player_scores[player_index], "\n")
         current_score = 0
 
         while True:
@@ -47,3 +48,8 @@ while max(player_scores) < max_score:
 
     player_scores[player_index] += current_score
     print("Your total score is: ", player_scores[player_index])
+
+max_score = max(player_scores)
+winning_index = player_scores.index(max_score)
+print("Player number", winning_index + 1,
+      "is the winner with a score of:", max_score)
